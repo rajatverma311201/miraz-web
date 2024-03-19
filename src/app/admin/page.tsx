1cf3669ff84bed660b7641b94e9d352b7d51444a
Rajat Verma <rajat311201@gmail.com>
2024-03-19 20:29:10 +0530

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@ import { db } from "@/lib/db";
 interface AdminHomePageProps {}
 
 const AdminHomePage: React.FC<AdminHomePageProps> = async ({}) => {
-    const res = await db.fAQ.findMany();
+    const res = await db.faq.findMany();
 
     return (
         <>
@@ -11,10 +11,10 @@ const AdminHomePage: React.FC<AdminHomePageProps> = async ({}) => {
             <hr />
             <h2>FAQ&apos;s</h2>
             <ul>
-                {res.map((faq) => (
-                    <li key={faq.id} className="mb-4">
-                        <p>{faq.question} </p>
-                        <span className="font-bold">{faq.answer}</span>
+                {res.map((item) => (
+                    <li key={item.id} className="mb-4">
+                        <p>{item.question} </p>
+                        <span className="font-bold">{item.answer}</span>
                     </li>
                 ))}
             </ul>
