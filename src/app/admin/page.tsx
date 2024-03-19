b8aae5c223a824cb09e5b3ca3fe60d7fadbd1d71
Rajat Verma <rajat311201@gmail.com>
2024-03-19 20:12:17 +0530

    db file

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,25 @@
+import { db } from "@/lib/db";
+
 interface AdminHomePageProps {}
 
-const AdminHomePage: React.FC<AdminHomePageProps> = ({}) => {
-    return <h1>AdminHomePage</h1>;
+const AdminHomePage: React.FC<AdminHomePageProps> = async ({}) => {
+    const res = await db.fAQ.findMany();
+
+    return (
+        <>
+            <h1>AdminHomePage</h1>
+            <hr />
+            <h2>FAQ&apos;s</h2>
+            <ul>
+                {res.map((faq) => (
+                    <li key={faq.id} className="mb-4">
+                        <p>{faq.question} </p>
+                        <span className="font-bold">{faq.answer}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 };
+
 export default AdminHomePage;
