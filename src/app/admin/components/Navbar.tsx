f9b303d89e224918dd5c308bc6efb8eeda3dbd75
Shubham <shubhampadhi271205@gmail.com>
2024-03-19 23:31:57 +0530

    Navbar layout done

--- src/app/admin/components/Navbar.tsx
+++ src/app/admin/components/Navbar.tsx
@@ -1,7 +1,19 @@
+import styles from './Navbar.module.css'
+import Link from 'next/link'
+
 export const Navbar = () => {
 	return (
 		<>
-			(nav)
+			<nav className={styles.navbar}>
+				<div className={styles.navLinks}>
+					<Link href="/" className={styles.navLink}>
+            Link1
+          </Link>
+          <Link href="/" className={styles.navLink}>
+            Link2
+          </Link>
+				</div>
+			</nav>
 		</>
 	);
 };
