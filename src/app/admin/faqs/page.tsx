c6bc110d73fc1dfed810444da3ab8a1957a8574d
sushant-wayal <sushantssr12345@gmail.com>
2024-03-20 11:57:11 +0530

    ui of admin home, events and faq and nav

--- src/app/admin/faqs/page.tsx
+++ src/app/admin/faqs/page.tsx
@@ -1,6 +1,45 @@
+import { Nav } from "@/components/nav";
+
 interface FaqsAdminPageProps {}
 
 const FaqsAdminPage: React.FC<FaqsAdminPageProps> = ({}) => {
-    return <h1>FaqsAdminPage</h1>;
+    const faqs = [
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+        {question: "Lorem ipsum dolor sit, amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, soluta. Animi voluptatem numquam explicabo fugit voluptate atque asperiores, accusantium exercitationem tempora, eveniet tenetur odio est hic magnam dolor odit rem?"},
+    ];
+    return (
+        <div className="flex flex-col items-center w-lvw gap-5">
+            <Nav />
+            <ul className="w-4/5 flex flex-col items-center gap-5">
+                {faqs.map(faq => {
+                    const { question, answer } = faq;
+                    return (
+                        <li key={question} className="w-full flex flex-col justify-between px-3 py-3 border-b-2 hover:border-b-4 border-x-4 border-black border-solid">
+                            <div className="border-b-2 border-black border-solid">
+                                <p> Question : </p>
+                                <p className="font-semibold"> {question} </p>
+                            </div>
+                            <div>
+                                <p> Answer : </p>
+                                <p className="font-semibold"> {answer} </p>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
 export default FaqsAdminPage;
---
+++ src/app/admin/speakers/page.tsx
@@ -0,0 +1,7 @@
+interface SpeakerAdminPageProps {}
+
+const SpeakerAdminPage: React.FC<SpeakerAdminPageProps> = ({}) => {
+    return <h1>SpeakerAdminPage</h1>;
+};
+
+export default SpeakerAdminPage;
