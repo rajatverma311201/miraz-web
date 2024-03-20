2c27764281ae3d19b0c8d7312d532c1fd1a9163f
sushant-wayal <sushantssr12345@gmail.com>
2024-03-20 15:49:47 +0530

    ui of all admin pages

--- src/app/admin/miraz-team/page.tsx
+++ src/app/admin/miraz-team/page.tsx
@@ -1,6 +1,38 @@
+import { Nav } from "@/components/nav";
+
 interface MirazTeamAdminPageProps {}
 
 const MirazTeamAdminPage: React.FC<MirazTeamAdminPageProps> = ({}) => {
-    return <h1>MirazTeamAdminPage</h1>;
+    const mirazTeamMenbers = [
+        {name: "Hackathon",email: "123@gmail.com", role: "volunteer"},
+        {name: "Singin Event",email: "123@gmail.com", role: "volunteer"},
+        {name: "Fashion Show",email: "123@gmail.com", role: "volunteer"},
+        {name: "Robo Fight", email: "123@gmail.com", role: "volunteer"},
+        {name: "Cricket Match",email: "123@gmail.com", role: "volunteer"},
+        {name: "Tug of war",email: "123@gmail.com", role: "volunteer"},
+        {name: "Speaker Talk", email: "123@gmail.com", role: "volunteer"},
+    ];
+    return (
+        <div className="flex flex-col items-center w-lvw gap-5">
+            <Nav />
+            <ul className="w-4/5 flex flex-col items-center gap-5">
+                <li className="w-full flex justify-between items-center px-3 py-3 h-5 border-4 border-black border-solid">
+                    <h2 className="font-bold">Name</h2>
+                    <p className="font-bold">Email</p>
+                    <p className="font-bold">Role</p>
+                </li>
+                {mirazTeamMenbers.map(mirazTeamMenber => {
+                    const { name, email, role } = mirazTeamMenber;
+                    return (
+                        <li key={email} className="w-full flex justify-between items-center px-3 py-3 h-5 border-b-2 hover:border-b-4 border-x-4 border-black border-solid">
+                            <p>{name}</p>
+                            <p>{email}</p>
+                            <p>{role}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );;
 };
 export default MirazTeamAdminPage;
--- src/app/admin/sponsors/page.tsx
+++ src/app/admin/sponsors/page.tsx
@@ -1,6 +1,40 @@
+import { Nav } from "@/components/nav";
+import Link from "next/link";
+
 interface SponsorsAdminPageProps {}
 
 const SponsorsAdminPage: React.FC<SponsorsAdminPageProps> = ({}) => {
-    return <h1>SponsorsAdminPage</h1>;
+    const sponsors = [
+        {name: "Hackathon", id: 1},
+        {name: "Singin Event", id: 4},
+        {name: "Fashion Show", id: 8},
+        {name: "Robo Fight", id: 18},
+        {name: "Cricket Match", id: 2},
+        {name: "Tug of war", id: 3},
+        {name: "Speaker Talk", id: 10},
+    ];
+    return (
+        <div className="flex flex-col items-center w-lvw gap-5">
+            <Nav />
+            <ul className="w-4/5 flex flex-col items-center gap-5">
+                <li className="w-full flex justify-between items-center px-3 py-3 h-5 border-4 border-black border-solid">
+                    <h2 className="font-bold">Name</h2>
+                    <p className="font-bold">Id</p>
+                </li>
+                {sponsors.map(sponsor => {
+                    const { name, id } = sponsor;
+                    return (
+                        <li key={id} className="w-full">
+                            <Link href={`/admin/sponsors/${id}`} className="w-full flex justify-between items-center px-3 py-3 h-5 border-b-2 hover:border-b-4 border-x-4 border-black border-solid">
+                                <p>{name}</p>
+                                <p>{id}</p>
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
+
 export default SponsorsAdminPage;
