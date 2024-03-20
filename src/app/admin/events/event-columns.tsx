b16563523831dacd506347e5cad0907d7c3a5ac2
sushant-wayal <sushantssr12345@gmail.com>
2024-03-21 03:25:56 +0530

    ui & CUD function of Events

---
+++ src/app/admin/events/event-columns.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Pencil } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
+import { DeleteAlertDialog } from "@/components/delete-alert-dialog";
+import { deleteEvent } from "@/actions/event";
+import { toast } from "sonner";
+
+export type Event = {
+    title: string;
+    id: string;
+};
+
+export const EventColumns: ColumnDef<Event>[] = [
+    {
+        accessorKey: "title",
+        header: "Title",
+    },
+    {
+        accessorKey: "id",
+        header: "Id",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const event = row.original;
+            const { id } = event;
+
+            const handleDeleteAction = async (onSuccess?: () => void) => {
+                const toastId = toast.loading("Deleting Event, Please Wait...");
+                try {
+                    await deleteEvent(id);
+                    toast.success("FAQ Deleted Successfully", { id: toastId });
+                    onSuccess?.();
+                } catch (error) {
+                    console.error(error);
+                    toast.error("Failed to delete FAQ", { id: toastId });
+                }
+            };
+
+            return (
+                <>
+                    <div className="flex gap-2">
+                        <Button asChild size={"iconSm"} variant={"outline"}>
+                            <Link href={`/admin/faqs/${id}`}>
+                                <Pencil size={16} />
+                            </Link>
+                        </Button>
+                        <DeleteAlertDialog action={handleDeleteAction} />
+                    </div>
+                </>
+            );
+        },
+    },
+];
