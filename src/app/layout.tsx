6cc38bdc79d24fd41d16049230ef196a39e64295
Sahil Rathore <sahilsinghrathore03@gmail.com>
2024-03-28 20:59:56 +0530

    Home Page UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
+import Image from "next/image";
+import background from "../../public/background.webp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +13,16 @@ export const metadata: Metadata = {
     title: "Miraz",
     description: "Fest of IIT Mandi 2024",
 };
+
+const pixelFont = localFont({
+    src: [
+        {
+            path: "./fonts/pixel_operator/PixelOperatorMono.ttf",
+            weight: "normal",
+            style: "normal",
+        },
+    ],
+});
 
 export default function RootLayout({
     children,
@@ -23,6 +36,17 @@ export default function RootLayout({
                     richColors={true}
                     theme="light"
                     position="top-center"
+                />
+                <Image
+                    alt="background"
+                    src={background}
+                    quality={100}
+                    fill
+                    sizes="100vw"
+                    className="-z-50"
+                    // style={{
+                    //     objectFit: "cover",
+                    // }}
                 />
                 {children}
             </body>
